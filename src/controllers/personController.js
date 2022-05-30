13fc9661ae5cfe5e4cf50421462575644cc08161
Luan Medeiros Silveira <49961064+luanmedeirossilveira@users.noreply.github.com>
2022-05-29 22:44:15 -0300

    Initial commit

---
+++ src/controllers/personController.js
@@ -0,0 +1,89 @@
+const { Client } = require('elasticsearch')
+
+const elasticClient = new Client({
+  host: 'http://localhost:9200'
+})
+
+exports.get = (req, res, next) => {
+    const searchText = req.query.text ? req.query.text : '' 
+
+    elasticClient.search({
+            body: {
+                query: {
+                    wildcard: { "name": `*${searchText.trim()}*` }
+                }
+            }
+        })
+        .then(response => {
+            return res.json(response)
+        }).catch(err => {
+            return res.status(500).json({ "message": err })
+        })
+
+};
+
+exports.getById = (req, res, next) => {
+    const id = req.params.id;
+
+    elasticClient.search({
+            body: {
+                query: {
+                    match: { "_id": id }
+                }
+            }
+        })
+        .then(response => {
+            return res.json(response)
+        }).catch(err => {
+            return res.status(500).json({ "message": err })
+        })
+};
+
+exports.post = (req, res, next) => {
+    elasticClient.index({
+        index: 'person',
+        body: {
+            "name": req.body.name,
+            "email": req.body.mail,
+            "age": req.body.age,
+        }
+    })
+    .then(() => {
+        return res.json({ "message": "Pessoa criada com sucesso!" })
+    }).catch(err => {
+        return res.status(500).json({ "message": err })
+    })
+};
+
+
+exports.put = (req, res, next) => {
+    const id = req.params.id;
+
+    elasticClient.update({
+            index: 'person',
+            id: id,
+            body: {
+                doc: req.body
+            }
+        })
+        .then(response => {
+            return res.json(response)
+        }).catch(err => {
+            return res.status(500).json({ "message": err })
+        })
+
+};
+
+exports.delete = (req, res, next) => {
+    const id = req.params.id;
+
+    elasticClient.delete({
+        index: 'person',
+        id: id
+    })
+        .then(response => {
+            return res.json(response);
+        }).catch(err => {
+            return res.status(500).json({ "message": err })
+        })
+};
